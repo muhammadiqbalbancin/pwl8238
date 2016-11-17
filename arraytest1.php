718af7f12841a8a918970684447e1fafff55f7ed
muhammadiqbalbancin <muhammad8238@students.amikom.ac.id>
2016-11-17 21:05:49 +0700

    tugas penganti praktikum

---
+++ arraytest1.php
@@ -0,0 +1,24 @@
+<?php
+$person = array('Aas', 'Jajang', 'Dadan', 'Renra');
+$pegawai = array('manajer'  => 'Aas',
+				 'dirut' 	=> 'JaJang',
+				 'Umum'     => 'Dadan',
+				 'teknisi'  => 'Renra');
+				 
+		foreach($pegawai as $name){
+			echo "Hallo, $name";
+			echo "<br />";
+		}
+		echo '<br />';
+		foreach($pegawai as $jabatan => $pejabat){
+			echo "$jabatan yang dibeban oleh $pejabat\n";
+			echo "<br />";
+		}
+?>
+<br />
+<?php
+	$ketemu = 20;
+	if ($ketemu < 21){
+		echo "true";
+	}
+?>
